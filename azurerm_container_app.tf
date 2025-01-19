36cd70c6e9cd38dbf5ea363a785046b39bfd43e6
Marcin Maruszewski <marcin.maruszewski@isobar.com>
2025-01-19 17:42:17 +0100

    Temporary remove mysql_flexible_server with dependencies.

--- azurerm_container_app.tf
+++ azurerm_container_app.tf
@@ -10,7 +10,6 @@ resource "azurerm_container_app_environment" "container_app_environment" {
   location                   = azurerm_resource_group.resource_group.location
   resource_group_name        = azurerm_resource_group.resource_group.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
-  infrastructure_subnet_id = [azurerm_subnet.subnet.id]
 }
 
 resource "azurerm_container_app" "container_app" {
